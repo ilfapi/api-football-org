b3b61b75d62d3369e2a112b7e9a35b69cca8ba11
ilfapi <116699810+ilfapi@users.noreply.github.com>
2023-03-28 17:11:46 +0700

    Update index.js

--- index.js
+++ index.js
@@ -17,6 +17,10 @@ app.get('/api/callback_zalo', (req, res) => {
   res.status(200).send({result: "ok"});
 });
 
+app.get('/zalo_verifierEVoRTxk38KnlXhL4i_aY773zemgSZ75MEJ8.html', (req, res) => {
+  res.sendFile(__dirname+"/zalo_verifierEVoRTxk38KnlXhL4i_aY773zemgSZ75MEJ8.html");
+});
+
 app.post('/api/matches', (req, res) => {
   var query = req.body;
   console.log(query);
